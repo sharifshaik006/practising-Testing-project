bce6182a05c6157264fe1d1ce6fba5be4594c97d
nagulshaikdigicatapult <128174943+nagulshaikdigicatapult@users.noreply.github.com>
2023-07-31 16:23:54 +0100

    DATA TYPE COMMIT SH

--- 02.variables.sh
+++ 02.variables.sh
@@ -2,3 +2,9 @@
 
 #learning about data types in scripting
 
+#mostly everything in the shell is string. When you pass the number it understand it as a number
+
+NUMBER1=200
+NUMBER2=300
+RESULT=${(NUMBER1+NUMBER2)}
+echo "addition of this ${RESULT}"
