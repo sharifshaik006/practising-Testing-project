2f866671b04340e1f30fedc393bc2e0a2f0ff588
nagulshaikdigicatapult <128174943+nagulshaikdigicatapult@users.noreply.github.com>
2023-07-31 14:30:58 +0100

    Commiting new file

---
+++ 01.variables.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NAME=SHAIK
+SURNAME=KING
+
+echo "this is ${NAME}"
+ECHO "THIS is ${kING}
+
+DATE=(date +%F)
+
+echo ${DATE}
+
+NAME=$1
+SURNAME=$2
+
+echo "what is the name ${NAME} and ${SURNAME}
+
+
+echo "what is username"
+
+read -s USERNAME
+
+echo "what is Password"
+
+read -s PASSWORD
