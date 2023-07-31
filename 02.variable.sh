a5d14c7f0bac8054ecfc4316449277a7bf890774
nagulshaikdigicatapult <128174943+nagulshaikdigicatapult@users.noreply.github.com>
2023-07-31 17:35:22 +0100

    data tpes sh

---
+++ 02.variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#data types in scripting are mostly strings
+#another data types are array which list of elements
+
+NUMBER1=200
+NUMBER2=399
+
+echo "adding is ${NUMBER1}+${NUMBER2}
+
+FRUITS=("apples grapes mangoes")
