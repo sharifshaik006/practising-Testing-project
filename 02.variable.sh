b0249bf842f33d3cf5e0ab295f079c0f7f99aff6
nagulshaikdigicatapult <128174943+nagulshaikdigicatapult@users.noreply.github.com>
2023-07-31 17:37:29 +0100

    DATA TYPES

--- 02.variable.sh
+++ 02.variable.sh
@@ -2,6 +2,7 @@
 
 #data types in scripting are mostly strings
 #another data types are array which list of elements
+# The elements start from index position 0 and 1 and 2 ........
 
 NUMBER1=200
 NUMBER2=399
@@ -9,3 +10,5 @@ NUMBER2=399
 echo "adding is ${NUMBER1}+${NUMBER2}
 
 FRUITS=("apples grapes mangoes")
+
+echo "the name of the 2element is ${FRUITS[2]}
