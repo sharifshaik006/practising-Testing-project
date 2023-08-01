be9e43cdfcd4dd94a2c82b0a70b22a992ab4b309
nagulshaikdigicatapult <128174943+nagulshaikdigicatapult@users.noreply.github.com>
2023-08-01 09:01:55 +0100

    New Variables.sh

--- 01.variables.sh
+++ 01.variables.sh
@@ -2,6 +2,9 @@
 
 #learning about variables in scripting
 #variable and inside the variable content as output DATE=Linux command 
+# arguments can be passed on and positioned through $1 $2 arguments
+# Encrypted information can be done by using read commands in the username and passwords in script
+
 
 NAME=SHAIK
 SURNAME=KING
