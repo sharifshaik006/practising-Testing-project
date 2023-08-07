4d2e363ecc9ff9f4fa89acbfc318a96a1c6f23a8
nagulshaikdigicatapult <128174943+nagulshaikdigicatapult@users.noreply.github.com>
2023-08-07 15:32:15 +0100

    p

--- Practisingshell.sh
+++ Practisingshell.sh
@@ -1,3 +1,11 @@
 #/bin/bash
 
-echo "Hello world"
+# echo "Hello world"
+#!/bin/bash
+
+# Ask the user for their name
+read -p "Please enter your name: " name
+
+# Print a personalized greeting
+echo "Hello, $name! Welcome to the world of shell scripting!"
+
