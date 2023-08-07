2f8f7201f95602cfa81fd71f317835660b4f80d2
nagulshaikdigicatapult <128174943+nagulshaikdigicatapult@users.noreply.github.com>
2023-08-07 10:39:50 +0100

    project.sh

---
+++ project1.sh
@@ -0,0 +1,7 @@
+# How to delete more than 2 weeks log files and schedule the script
+
+
+# For every task if we make it understandable by following Alogorithm Splitting the tasks into steps will help us
+
+# 2 weeks ago files ........> FIND  HAVE TO FIND WHICH FOLDER TO DELETE WHICH .LOG FILES TO DELETE FINALLY SCHEDULE THE SCRIPT
+
