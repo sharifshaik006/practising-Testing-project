9a52110a3aaf69cf37d36a166c264918ecc3caf6
nagulshaikdigicatapult <128174943+nagulshaikdigicatapult@users.noreply.github.com>
2023-08-08 10:43:20 +0100

    p

--- Practisingshell.sh
+++ Practisingshell.sh
@@ -1,11 +1,15 @@
-#/bin/bash
-
-# echo "Hello world"
 #!/bin/bash
 
-# Ask the user for their name
-read -p "Please enter your name: " name
+# Variables
+PARTITION="/dev/disk3s1s1"
+THRESHOLD=4  # 4% used space threshold. You might want to set it to 80 because you are checking used space.
+EMAIL="sharifshaik006@gmail.com"
+ALERT_MESSAGE="Warning! Disk space on $PARTITION is below the threshold."
 
-# Print a personalized greeting
-echo "Hello, $name! Welcome to the world of shell scripting!"
+# Get the available disk space as a percentage
+USED_SPACE=$(df | grep "$PARTITION" | awk '{print $5}' | sed 's/%//')
 
+# Compare and send alert if below threshold
+if (( $USED_SPACE > $THRESHOLD )); then
+    echo "$ALERT_MESSAGE" | mail -s "Disk Space Alert" "$EMAIL"
+fi
