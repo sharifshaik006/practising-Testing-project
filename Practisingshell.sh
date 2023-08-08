42988ce462e28b09b1b87b5b2e6d16454239bc85
nagulshaikdigicatapult <128174943+nagulshaikdigicatapult@users.noreply.github.com>
2023-08-08 10:46:01 +0100

    pp

--- Practisingshell.sh
+++ Practisingshell.sh
@@ -2,7 +2,7 @@
 
 # Variables
 PARTITION="/dev/disk3s1s1"
-THRESHOLD=4  # 4% used space threshold. You might want to set it to 80 because you are checking used space.
+THRESHOLD=4
 EMAIL="sharifshaik006@gmail.com"
 ALERT_MESSAGE="Warning! Disk space on $PARTITION is below the threshold."
 
@@ -10,6 +10,6 @@ ALERT_MESSAGE="Warning! Disk space on $PARTITION is below the threshold."
 USED_SPACE=$(df | grep "$PARTITION" | awk '{print $5}' | sed 's/%//')
 
 # Compare and send alert if below threshold
-if (( $USED_SPACE > $THRESHOLD )); then
+if (( USED_SPACE > THRESHOLD )); then
     echo "$ALERT_MESSAGE" | mail -s "Disk Space Alert" "$EMAIL"
 fi
